5ae48bdcc1c2e24f0596754d968bb2184ad274ac
hasibulislam999 <hasib143sl@gmail.com>
2022-12-07 22:24:32 +0600

    optimize information due to about section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,121 +1,79 @@
 import React from "react";
 
 const About = () => {
+  const information = [
+    {
+      title: "Shooting Stars",
+      description:
+        "Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.",
+    },
+    {
+      title: "The Catalyzer",
+      description:
+        "Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.",
+    },
+    {
+      title: "Neptune",
+      description:
+        "Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.",
+    },
+    {
+      title: "Melanchole",
+      description:
+        "Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.",
+    },
+  ];
+
   return (
     <section className="container mx-auto">
       <section className="flex flex-col gap-y-8 py-4">
-        <section class="text-gray-600 body-font bg-gray-100 rounded-2xl">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-col text-center w-full mb-20">
-              <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+        <section className="text-gray-600 body-font bg-gray-100 rounded-2xl">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
                 ROOF PARTY POLAROID
               </h2>
-              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
                 Master Cleanse Reliac Heirloom
               </h1>
-              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                 Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
                 gentrify, subway tile poke farm-to-table. Franzen you probably
                 haven't heard of them man bun deep jianbing selfies heirloom
                 prism food truck ugh squid celiac humblebrag.
               </p>
             </div>
-            <div class="flex flex-wrap">
-              <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                  Shooting Stars
-                </h2>
-                <p class="leading-relaxed text-base mb-4">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-                <a href="/" class="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-              <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                  The Catalyzer
-                </h2>
-                <p class="leading-relaxed text-base mb-4">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-                <a href="/" class="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-              <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                  Neptune
-                </h2>
-                <p class="leading-relaxed text-base mb-4">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-                <a href="/" class="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-              <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                  Melanchole
-                </h2>
-                <p class="leading-relaxed text-base mb-4">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-                <a href="/" class="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
+            <div className="flex flex-wrap">
+              {information.map((info, index) => (
+                <div
+                  key={index}
+                  className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60"
+                >
+                  <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+                    {info.title}
+                  </h2>
+                  <p className="leading-relaxed text-base mb-4">
+                    {info.description}
+                  </p>
+                  <span className="text-indigo-500 inline-flex items-center cursor-help">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </span>
+                </div>
+              ))}
             </div>
             <a
-              class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg w-fit"
+              className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg w-fit"
               href="#contact-us"
             >
               Contact us
@@ -123,8 +81,8 @@ const About = () => {
           </div>
         </section>
         {/* mapped section */}
-        <section class="text-gray-600 body-font relative">
-          <div class="absolute inset-0 bg-gray-300 rounded-2xl">
+        <section className="text-gray-600 body-font relative">
+          <div className="absolute inset-0 bg-gray-300 rounded-2xl">
             <iframe
               width="100%"
               height="100%"
@@ -140,39 +98,39 @@ const About = () => {
               }}
             ></iframe>
           </div>
-          <div class="container px-5 py-24 mx-auto flex" id="contact-us">
-            <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-              <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+          <div className="container px-5 py-24 mx-auto flex" id="contact-us">
+            <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
                 Feedback
               </h2>
-              <p class="leading-relaxed mb-5 text-gray-600">
+              <p className="leading-relaxed mb-5 text-gray-600">
                 Post-ironic portland shabby chic echo park, banjo fashion axe
               </p>
-              <div class="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-600">
+              <div className="relative mb-4">
+                <label for="email" className="leading-7 text-sm text-gray-600">
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
                   name="email"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
-              <div class="relative mb-4">
-                <label for="message" class="leading-7 text-sm text-gray-600">
+              <div className="relative mb-4">
+                <label for="message" className="leading-7 text-sm text-gray-600">
                   Message
                 </label>
                 <textarea
                   id="message"
                   name="message"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                 ></textarea>
               </div>
-              <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Button
               </button>
-              <p class="text-xs text-gray-500 mt-3">
+              <p className="text-xs text-gray-500 mt-3">
                 Chicharrones blog helvetica normcore iceland tousled brook viral
                 artisan.
               </p>
