b2f6027ed163ad1810255cf50a764848d0d1bd20
hasibulislam999 <hasib143sl@gmail.com>
2022-12-08 08:14:14 +0600

    modified throughout adding product lists

--- client/src/pages/TopRated.js
+++ client/src/pages/TopRated.js
@@ -1,7 +1,27 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const TopRated = () => {
-  return <section>This is Top Rated route</section>;
+  const {
+    state: { products, loading, error },
+  } = useProducts();
+
+  let content = null;
+  if (loading) content = <p>Loading...</p>;
+  if (error) content = <p>Something went wrong...</p>;
+  if (!loading && !error && products.length === 0)
+    content = <p>Nothing to show...</p>;
+  if (!loading && !error && products.length)
+    content = products
+      .filter((product) => product.rating >= 4)
+      .map((product) => <ProductCard key={product._id} product={product} />).reverse();
+
+  return (
+    <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-8 my-10">
+      {content}
+    </section>
+  );
 };
 
 export default TopRated;
