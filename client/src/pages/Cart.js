a12e78c76e0250385ece4a74d7a2663693f0c225
hasibulislam999 <hasib143sl@gmail.com>
2022-12-08 13:01:41 +0600

    implement cart and wishlist functionalities

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,7 +1,27 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const Cart = () => {
-  return <section>This is Cart route</section>;
+  const {
+    state: { cart, loading, error },
+  } = useProducts();
+
+  let content = null;
+  if (loading) content = <p>Loading...</p>;
+  if (error) content = <p>Something went wrong...</p>;
+  if (!loading && !error && cart?.length === 0)
+    content = <p>Nothing to show...</p>;
+  if (!loading && !error && cart?.length)
+    content = cart
+      .map((crt) => <ProductCard key={crt._id} product={crt} />)
+      .reverse();
+
+  return (
+    <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-8 my-10">
+      {content}
+    </section>
+  );
 };
 
 export default Cart;
--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,7 +1,27 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const Wishlist = () => {
-  return <section>This is Wishlist route</section>;
+  const {
+    state: { wishlist, loading, error },
+  } = useProducts();
+
+  let content = null;
+  if (loading) content = <p>Loading...</p>;
+  if (error) content = <p>Something went wrong...</p>;
+  if (!loading && !error && wishlist?.length === 0)
+    content = <p>Nothing to show...</p>;
+  if (!loading && !error && wishlist?.length)
+    content = wishlist
+      .map((list) => <ProductCard key={list._id} product={list} />)
+      .reverse();
+
+  return (
+    <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-8 my-10">
+      {content}
+    </section>
+  );
 };
 
 export default Wishlist;
